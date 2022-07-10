e287ddc059d20b33f4da9ecf515f95ecf527b4ec
Davoudkhodaverdian <davoudkhodaverdian20@gmail.com>
2022-07-11 00:53:41 +0430

    change menubar

--- components/dashboard/dashboard.js
+++ components/dashboard/dashboard.js
@@ -13,7 +13,7 @@ function Dashbord({ children }) {
 
     return (
         <div className='pr-0 sm:flex flex-row overflow-hidden '>
-            <Menubar sidebar={sidebar} sendSidebarState={sendSidebarState} />
+          
             <div className="flex flex-col w-full  md:pr-48">
                 <Header sendSidebarState={sendSidebarState} sidebar={sidebar} />
                 <div className="w-full max-w-7xl mx-auto p-3">
